146bfd675992e5ea0f211d25e68e7d855b4fbf55
Dheeraj Abhishek <dheerajabhishek111@gmail.com>
2025-02-11 00:15:35 +0530

    updated with email notification reminders

--- Medication_Reminder/index.js
+++ Medication_Reminder/index.js
@@ -25,9 +25,12 @@ const userschema = {
     password: String,
     category: String
 }
-const User = mongoose.model("User", userschema);
+const schedule = require("node-schedule");
+const nodemailer = require("nodemailer");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+const User = mongoose.model("User", userschema);
 app.post("/signUp", (req, res) => {
     const { name, phone, email, password, category } = req.body;
     const user = new User({ userName: name, contactNum: phone, email: email, password: password, category: category, });
@@ -63,16 +66,6 @@ app.post("/login", async (req, res) => {
     catch (error) {
         res.status(500).send("Inter issue please try later")
     }
-    //         if (!find) {
-    //             return res.status(404).send("No user found")
-    //         }
-    //         else {
-    //             return res.status(200).send("user found")
-    //         }
-
-    //     } catch (error) {
-    //         res.status(500).send("Inter issue please try later")
-    //     }
 
 })
 const infantschema = {
@@ -81,14 +74,19 @@ const infantschema = {
     vaccineName: String,
     medicineName: String,
     days: Number,
+    time: String,
     slots: Number
 }
 const Infant = mongoose.model("Infant", infantschema);
 app.post("/infant", (req, res) => {
-    const { vaccineDate, vaccineName, medicineName, days, slots } = req.body;
-
-    const user = new Infant({ userid: userid, vaccineName: vaccineName, vaccineDate: vaccineDate, medicineName: medicineName, days: days, slots: slots });
-    const id = user.userid
+    const { vaccineDate, vaccineName, medicineName, days, slots, time } = req.body;
+    const user = new Infant({ userid: userid, vaccineName: vaccineName, vaccineDate: vaccineDate, medicineName: medicineName, days: days, time: time, slots: slots });
+    const id = user.userid;
+    let Time = user.time;
+    console.log(Time)
+    let hrs = Time.slice(0, 2)
+    let mins = Time.slice(3)
+    console.log(`at time ${hrs}hours and ${mins}minutes`)
     user.save()
         .then((data) => {
             console.log("inserted");
@@ -97,7 +95,46 @@ app.post("/infant", (req, res) => {
         .catch((e) => {
             console.log("OOPS ERROR!! ", e)
         })
-    res.render("home", { uid: id })
+    let email = "";
+    async function mail() {
+        const find = await User.findOne({ userid: id })
+        email = find.email;
+        console.log("this is email", email)
+
+        // Configure the transporter
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "medicationreminder.notify@gmail.com",
+                pass: "jeoi mqni tvea bphe" // App Password
+            }
+        });
+
+        // Function to schedule email
+        function scheduleEmail(email, time) {
+            schedule.scheduleJob(time, function () {
+                const mailOptions = {
+                    from: "medicationreminder.notify@gmail.com",
+                    to: email,
+                    subject: "Medication Reminder 💊",
+                    text: "Hey! Don't forget to take your medication today. Stay healthy! "
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log("Error:", error);
+                    } else {
+                        console.log(`Email sent to ${email} at ${time}:`, info.response);
+                    }
+                });
+            });
+        }
+
+        scheduleEmail(email, `${mins} ${hrs}  * * *`); // CRON format (minute hour day month dayOfWeek)
+    }
+    mail();
+    res.render("home", { uid: id });
+
 });
 app.get('/profile/:id', async (req, res) => {
     const { id } = req.params
